2edde4e2b7db527ee7a75e3dcae0fa51b4b2f7f3
Rachell Sincere <rachellsincere@gmail.com>
2019-12-15 20:59:59 -0500

    completed tabs component and axios request

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,32 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+const topicsDiv = document.querySelector('.topics'); //selected tabs div to use later
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then( response => {
+        // Remember response is an object
+        console.log(response);
+        const data = response.data.topics;
+        data.forEach((topic) => { //iterate through array list received to return component
+            topicsDiv.appendChild(newTab(topic));
+        });
+
+    })
+    .catch( error => {
+        console.log("Error:", error);
+    })
+
+
+
+function newTab(item) {
+        const tabDiv = document.createElement('div'); //html structure
+        tabDiv.classList.add('tab'); //added classes in
+        tabDiv.textContent = item; //set text inside of div
+        
+        return tabDiv;
+
+}
+
+
